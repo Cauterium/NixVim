20ce14cda905b6874e7358b5560b6f487d20065d
Fabian Brenneisen <brenneisen.fabian@gmail.com>
2025-05-17 11:37:49 +0200

    fix(lualine): fix option for enabling lualine

--- config/plugins/editor.nix
+++ config/plugins/editor.nix
@@ -2,7 +2,7 @@
   plugins = {
     illuminate.enable = true;
     indent-blankline.enable = true;
-    lua-line.enable = true;
+    lualine.enable = true;
     neo-tree.enable = true;
     todo-comments.enable = true;
     treesitter.enable = true;
